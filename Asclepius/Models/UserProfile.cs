1f62e8003a9510e56ff0a8526c213bd23c3ea202
Tasha Lane <designtmg77@gmail.com>
2020-10-23 11:18:12 -0500

    UserProfile.cs Model

--- Asclepius/Models/UserProfile.cs
+++ Asclepius/Models/UserProfile.cs
@@ -7,5 +7,18 @@ namespace Asclepius.Models
 {
     public class UserProfile
     {
+        public int Id { get; set; }
+
+        //[Required]
+        public string Name { get; set; }
+
+        //[Required]
+        public string Email { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        //[Required]
+        public DateTime DateCreated { get; set; }
+
     }
 }
